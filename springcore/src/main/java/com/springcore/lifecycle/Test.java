ddb19ada6d02e66ee33e000001deaf2852955bb9
Sanskriti-sing <sanskriti12340987@gmail.com>
2024-01-01 04:04:17 +0530

    Using Annotations further in Example

--- springcore/src/main/java/com/springcore/lifecycle/Test.java
+++ springcore/src/main/java/com/springcore/lifecycle/Test.java
@@ -8,13 +8,16 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
-		Samosa s1 = (Samosa) context.getBean("s1");
-		context.registerShutdownHook();
-		System.out.println(s1);
+//		Samosa s1 = (Samosa) context.getBean("s1");
+//		context.registerShutdownHook();
+//		System.out.println(s1);
+//		
+//		System.out.println("**********");
+//		pepsi p1 = (pepsi) context.getBean("p1");
+//		System.out.println(p1);
 		
-		System.out.println("**********");
-		pepsi p1 = (pepsi) context.getBean("p1");
-		System.out.println(p1);
+		ExampleAnnotation example = (ExampleAnnotation) context.getBean("example");
+		System.out.println(example);
 	}
 
 }
